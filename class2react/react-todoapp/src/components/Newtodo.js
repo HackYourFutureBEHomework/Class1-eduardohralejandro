72add2c4b31b361d8aa89f4f60b985893885dfc8
eduardo hernandez <eduardohralejandro@gmail.com>
2018-10-17 23:15:05 +0200

    new feature edit by clicking inline text

--- class2react/react-todoapp/src/components/Newtodo.js
+++ class2react/react-todoapp/src/components/Newtodo.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import Moment from "react-moment";
 import DatePicker from "react-date-picker";
+import EditableLabel from "react-inline-editing";
 class Newtodo extends Component {
   constructor() {
     super();
+    this._handleFocus = this._handleFocus.bind(this);
+    this._handleFocusOut = this._handleFocusOut.bind(this);
     this.state = {
       newTodo: "",
+      message: "ReactInline demo",
       todos: [
         {
           title: "Get out of bed",
@@ -24,6 +28,14 @@ class Newtodo extends Component {
         }
       ]
     };
+  }
+
+  _handleFocus(text) {
+    console.log("Focused with text: " + text);
+  }
+
+  _handleFocusOut(text) {
+    console.log("Left editor with text: " + text);
   }
 
   onsubmit = e => {
@@ -75,6 +87,7 @@ class Newtodo extends Component {
             placeholder="Enter todo"
             value={this.state.newTodo}
           />
+
           <DatePicker
             className="date"
             onChange={this.onChange}
@@ -96,10 +109,20 @@ class Newtodo extends Component {
                   style={{
                     textDecoration: todo.done ? "line-through" : "inherit"
                   }}
-                >
-                  {todo.title}
-                </span>{" "}
+                />{" "}
                 <span />
+                <EditableLabel
+                  text={todo.title}
+                  labelClassName="myLabelClass"
+                  inputClassName="myInputClass"
+                  inputWidth="200px"
+                  inputHeight="25px"
+                  inputMaxLength="50"
+                  labelFontWeight="bold"
+                  inputFontWeight="bold"
+                  onFocus={this._handleFocus}
+                  onFocusOut={this._handleFocusOut}
+                />
                 it was posted on <span />
                 <Moment format="YYYY/MM/DD">
                   {this.props.dateToFormat}
